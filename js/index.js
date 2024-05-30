2db22ab19e04445b50cf9f20709ffb141505dae3
Magdy Abas <132621536+magdy-abas@users.noreply.github.com>
2024-05-30 06:21:40 +0300

    vision1.3

--- js/index.js
+++ js/index.js
@@ -107,3 +107,37 @@ itemHoverFour.forEach((itemHover) => {
     dropdownBtnFour.style.backgroundColor = "#eee";
   });
 });
+
+// modal in contact us
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.getElementsByClassName("thumbnail");
+var modalImg = document.getElementById("img01");
+
+for (var i = 0; i < images.length; i++) {
+  images[i].onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    document.body.classList.add("modal-open"); // Disable body scroll
+  };
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  document.body.classList.remove("modal-open"); // Enable body scroll
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.body.classList.remove("modal-open"); // Enable body scroll
+  }
+};
